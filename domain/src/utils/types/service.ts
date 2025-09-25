8164dcc6a64d69776343bfcd78bf7820efecec4c
angelggc <angelgimenez992@gmail.com>
2025-09-25 20:08:45 -0300

    implemente serviceQuery and serviceStorage

--- domain/src/utils/types/service.ts
+++ domain/src/utils/types/service.ts
@@ -1,9 +1,17 @@
 import type { Entity } from "./entity.js";
 
-export interface Service<TEntity extends Entity> {
+export interface Service<TEntity extends Entity>
+  extends ServiceQuery<TEntity>,
+    ServiceStorage<TEntity> {}
+
+interface ServiceQuery<TEntity extends Entity> {
   findById: (id: string) => Promise<TEntity | undefined>;
   findAll: () => Promise<TEntity[]>;
+}
+
+interface ServiceStorage<TEntity extends Entity> {
   editOne: (data: TEntity) => Promise<TEntity>;
   save: (data: TEntity) => Promise<void>;
   updateMany: (data: TEntity[]) => Promise<TEntity[] | undefined>;
+  delete: (id: string) => Promise<void>;
 }
